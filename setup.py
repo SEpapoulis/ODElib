5599a9fb0ad5b9a806a6bb51643bc232e701d2c9
Spiridon Papoulis <spapouli@vols.utk.edu>
2021-02-12 11:08:53 -0500

    install requirements and setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ODElib',
+      version='0.1',
+      description='Python Distribution Utilities',
+      author='Spiridon Papoulis',
+      author_email='spapouli@vols.utk.edu',
+      url='https://github.com/SEpapoulis/ODElib',
+      packages=['ODElib'],
+     )
